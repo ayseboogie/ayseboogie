2393e4eec2167e06c8e8f7acb43127827677937b
Ayse Stinnett <aysestinnett@Ayses-MBP.attlocal.net>
2020-11-30 13:22:53 -0500

    configuration

---
+++ pages/components/header.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Link from "next/link";
+
+function header() {
+    const [isExpanded, toggleExpansion] = useState(false);
+
+    return (
+        <nav className="flex items-center justify-between flex-wrap bg-black p-6">
+            <div className="flex items-center flex-shrink-0 text-white mr-6">
+                {/* put logo here */}
+                <span className="font-semibold text-xl tracking-tight">"artists"</span>
+            </div>
+            <div className="block lg:hidden">
+                <button
+                    onClick={() => toggleExpansion(!isExpanded)}
+                    className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
+                >
+                    <svg
+                        className="fill-current h-3 w-3"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <title>Menu</title>
+                        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                    </svg>
+                </button>
+            </div>
+            <div
+                className={`${
+                    isExpanded ? `block` : `hidden`
+                } w-full block flex-grow lg:flex lg:items-center lg:w-auto`}
+            >
+                <div className="flex justify-end items-center text-sm lg:flex-grow">
+          <span className="block lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 text-white">
+            <Link href="/artists">
+              <a>Artists</a>
+            </Link>
+          </span>
+                    <span className="block lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 text-white">
+            <Link href="/music">
+              <a>Music</a>
+            </Link>
+          </span>
+                    <span className="block lg:inline-block lg:mt-0 text-teal-200 hover:text-white text-white">
+            <Link href="/videos">
+              <a>Vidoes</a>
+            </Link>
+          </span>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default header;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
-import Head from 'next/head'
+import React from "react";
+
+import { useRouter } from 'next/router'
+
 
 export default function Home() {
+    const router = useRouter()
+    const Fall = "/Fall.png";
+
   return (
-    <div className="container">
-
+      <div className="image">
+{/*<img src={Fall} />*/}
      <div>My site</div>
-
+        <span onClick={() => router.push('/contact')}>Click me</span>
       <footer>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -16,6 +22,20 @@ export default function Home() {
           <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
         </a>
       </footer>
+        <style jsx>
+            {`
+          .image {
+            position: absolute;
+            top: 0;
+            right: 0;
+            left: 0;
+            height: 100%;
+            background-size: cover;
+            background-position: center;
+            background-image: url(${Fall});
+          }
+        `}
+        </style>
     </div>
   )
 }
