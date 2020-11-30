457bf66d83623bef9cb2ed14e048384662135371
Ayse Stinnett <aysestinnett@Ayses-MBP.attlocal.net>
2020-11-29 23:08:20 -0500

    project setup complete..next js with tailwind

---
+++ pages/contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function contact() {
+    return (
+        <>
+            <div className="text-white" style={{backgroundColor: "black", height: "100vh"}}>
+               hjh
+            </div>
+        </>
+    )
+}
+
+export default contact;
